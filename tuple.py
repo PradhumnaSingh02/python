72b755b886c1c52b83fd4ffa565aea7d88bd867d
PradhumnaSingh02 <89626211+PradhumnaSingh02@users.noreply.github.com>
2021-10-03 12:51:41 +0530

    Tuple

---
+++ tuple.py
@@ -0,0 +1,36 @@
+#creating a tuple withinteger and string......
+#Tuple1=(1,2,'3')
+#$print(Tuple1)
+#Creating empty tuple
+t2=()
+print(t2)
+#Creating tuple with use of Strings
+t3=('Hello','World')
+print(t3)
+#Creating tuple with help of list
+list=[1,2,3,'de',4,5]
+print(tuple(list))
+#concatenation of tuple
+Tuple1=(1,2,3)
+Tuple2=(6,5,4)
+t=Tuple1+Tuple2
+print(t)
+# Slicing of Tuple..............
+p='HelloDuniya'
+print(p[1:])
+print(p[::-1])
+print(p[4:9])
+#Deleting a tuple
+k=(1,2,3,4,5,5,5,6,7)
+print(len(k))
+print(max(k))
+print(min(k))
+print(sum(k))
+print(sorted(k))
+print(any(k))
+print(counter(k))
+del k
+print(k)
+
+
+
